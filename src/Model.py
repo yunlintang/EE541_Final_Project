e29de3f58ea5b830354c986ffca779615b0c986d
Gary <yzheng01@usc.edu>
2022-05-11 02:27:58 -0700

    pytorch_model

---
+++ src/Model.py
@@ -0,0 +1,136 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest, f_classif
+from torch.optim.lr_scheduler import StepLR
+import torchvision
+import torchvision.transforms as transforms
+import torch.utils.data as Data
+from torch.utils.tensorboard import SummaryWriter
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error as MSE
+
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
+
+import matplotlib.pyplot as plt
+
+
+def linear_regression_task(train_x, train_y, test_x, test_y):
+    model_linear = LinearRegression()
+    # define feature selection 43
+    # fs = SelectKBest(score_func=f_classif, k=2000)
+    # x_train_select = fs.fit_transform(train_x, train_y)
+    # x_test_select = fs.transform(test_x)
+
+    pca_full = PCA(n_components=1500)
+    pca_full.fit(train_x)
+    x_train_select = pca_full.transform(train_x)
+    x_test_select = pca_full.transform(test_x)
+
+    model_linear.fit(x_train_select, train_y)
+    y_linear_hat1 = model_linear.predict(x_test_select)
+    rmse_linear1 = MSE(test_y, y_linear_hat1)**0.5
+    r2_linear1 = model_linear.score(x_test_select, test_y)
+    print(f"linear regression —— RMSE:{rmse_linear1}, R-squared:{r2_linear1}")
+
+
+class Net(nn.Module):
+    def __init__(self, n_input, n_hidden=(500, 500), n_output=1, drop_out=0.3):
+        super(Net, self).__init__()
+        self.hidden1 = nn.Linear(n_input, n_hidden[0])
+        self.hidden2 = nn.Linear(n_hidden[0], n_hidden[1])
+        self.output = nn.Linear(n_hidden[1], n_output)
+        self.dropout = nn.Dropout(p=drop_out)
+
+    def forward(self, input_data):
+        out = F.relu(self.hidden1(input_data))
+        out = self.dropout(out)
+        out = F.relu(self.hidden2(out))
+        out = self.dropout(out)
+        out = self.output(out)
+        return out
+
+
+def train(train_x, train_y, test_x, test_y):
+    # pca_full = PCA(n_components=1500)
+    # pca_full.fit(train_x)
+    # x_train_select = pca_full.transform(train_x)
+    # x_test_select = pca_full.transform(test_x)
+
+    # define feature selection 43
+    fs = SelectKBest(score_func=f_classif, k=1500)
+    x_train_select = fs.fit_transform(train_x, train_y)
+    x_test_select = fs.transform(test_x)
+
+    learning_rate = 0.0005
+    epochs = 40
+    batch_size = 100
+    features_num = x_train_select.shape[1]
+    writer = SummaryWriter("../NN_model")
+
+    # tensor_x = torch.Tensor(train_x.to_numpy())
+    tensor_x = torch.Tensor(x_train_select)
+    tensor_y = torch.Tensor(train_y.to_numpy())
+    torch_train = Data.TensorDataset(tensor_x, tensor_y)
+    train_loader = Data.DataLoader(
+        dataset=torch_train,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    # tensor_test_x = torch.Tensor(test_x.to_numpy())
+    tensor_test_x = torch.Tensor(x_test_select)
+    tensor_test_y = torch.Tensor(test_y.to_numpy())
+    # torch_test = Data.TensorDataset(tensor_test_x, tensor_test_y)
+    # test_loader = Data.DataLoader(
+    #     dataset=torch_test,
+    #     batch_size=batch_size,
+    #     shuffle=True
+    # )
+
+    model = Net(n_input=features_num)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.0001)
+    loss_func = nn.MSELoss()
+    scheduler = StepLR(optimizer, step_size=20, gamma=0.5)
+
+    for ep in range(epochs):
+        loss_ep = 0
+        N = 0
+        for sample_feature, score in train_loader:
+            y_hat = model(sample_feature)
+            loss = loss_func(y_hat, score)
+            loss_ep += loss
+            N += 1
+            optimizer.zero_grad()
+            loss.backward()
+
+            optimizer.step()
+        scheduler.step()
+        writer.add_scalar(tag="Train_loss", scalar_value=loss_ep/N, global_step=ep)
+        for name, param in model.named_parameters():
+            writer.add_histogram(tag=name + '_data', values=param.data, global_step=ep)
+        print(f'Epoch:{ep}, loss: {loss_ep / N}, step:{scheduler.get_lr()}')
+    for name, param in model.named_parameters():
+        writer.add_histogram(tag='Final_' + name + '_data', values=param.data)
+
+    torch.save(model.state_dict(), "../Model_param")
+    print("Finished Training")
+
+    with torch.no_grad():
+        model.eval()
+        outputs = model(tensor_test_x)
+        r2 = r2_score(tensor_test_y, outputs)
+        rmse = MSE(tensor_test_y, outputs) ** 0.5
+        print(f"R-2:{r2}, RMSE:{rmse}")
+        pre = outputs.tolist()
+        true = tensor_test_y.tolist()
+        plt.title("Movie ratings vs Predicted ratings")
+        plt.xlabel("movie ratings")
+        plt.ylabel("predicted ratings")
+        plt.plot(true, pre, 'o', color="b")
+
+    plt.axline((0, 0), slope=1, color="r", ls="--", lw=2.5)
+    plt.show()
--- src/main_.py
+++ src/main_.py
@@ -1,96 +1,65 @@
-import astropy
-from ast import literal_eval
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MultiLabelBinarizer
 import warnings
-import data_clean as proc
-import feature_engineering as engineering
-from collections import Counter
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
-import torchvision.transforms as transforms
-import torch.utils.data as Data
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error as MSE
+
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
+
+import Model
 
 warnings.filterwarnings('ignore')
 
-class Net(nn.Module):
-    def __init__(self, n_input, n_hidden=(1000, 500), n_output=1, drop_out=0.3):
-        super(Net, self).__init__()
-        self.hidden1 = nn.Linear(n_input, n_hidden[0])
-        self.hidden2 = nn.Linear(n_hidden[0], n_hidden[1])
-        self.output = nn.Linear(n_hidden[1], n_output)
-        self.dropout = nn.Dropout(p=drop_out)
 
-    def forward(self, input_data):
-        out = F.relu(self.hidden1(input_data))
-        out = self.dropout(out)
-        out = F.relu(self.hidden2(out))
-        out = self.dropout(out)
-        out = self.output(out)
-        return out
+def svr_task(train_x, train_y, test_x, test_y):
+    # pca_full = PCA(n_components=35)
+    # pca_full.fit(train.drop(['G1', 'G2', 'G3'], axis=1))
+    # x_train_select = pca_full.transform(train.drop(['G1', 'G2', 'G3'], axis=1))
+    # x_test_select = pca_full.transform(test.drop(['G1', 'G2', 'G3'], axis=1))
+    #
+    # pca_full.fit(train.drop(['G3'], axis=1))
+    # x_train_select_3 = pca_full.transform(train.drop(['G3'], axis=1))
+    # x_test_select_3 = pca_full.transform(test.drop(['G3'], axis=1))
 
+    # define feature selection 43
+    # fs = SelectKBest(score_func=f_classif, k=1500)
+    # x_train_select = fs.fit_transform(train_x, train_y)
+    # x_test_select = fs.transform(test_x)
 
+    c_list = []
+    for i in range(1, 2):
+        c_list.append(i * 10)
 
-# data transformation
-# OUT_DATA = "src/data/interim/"
-# RAW_DATA = "src/data/raw/"
-# proc.PreprocessMM(RAW_DATA, OUT_DATA, save=True)
+    gamma_list = []
+    for j in range(1, 2):
+        gamma_list.append(j * 0.001)
 
+    # cross validation
+    param_grid = {'gamma': gamma_list, 'C': c_list, 'kernel': ['rbf']}
+    # model = GridSearchCV(SVR(), param_grid, cv=10, scoring='r2')
 
-META_DATA = "data/interim/movies_metadata_clean.csv"
-CREDITS_DATA = "data/interim/credits_clean.csv"
-KEYWORDS_DATA = "data/interim/keywords_clean.csv"
-COMBINE_DATA = "data/interim/combine.csv"
-meta_data = pd.read_csv(META_DATA)
-credits_data = pd.read_csv(CREDITS_DATA)
-keywords_data = pd.read_csv(KEYWORDS_DATA)
-combine_data = pd.read_csv(COMBINE_DATA)
+    model = SVR(kernel='rbf', C=1000, gamma=0.1)
+    model.fit(train_x, train_y)
 
-COMBINE_DATA_FINAL = "data/interim/combine_clean_oh.csv"
-combine_data_final = pd.read_csv(COMBINE_DATA_FINAL)
-new_col = [col for col in combine_data_final.columns if col != 'score'] + ['score']
-combine_data_final = combine_data_final[new_col]
-combine_data_final = combine_data_final.drop(['id', 'keywords', 'overview', 'title'], axis=1)
+    # mission 1
+    model.fit(train_x, train_y)
+    y_pre1 = model.predict(test_x)
+    rmse1 = MSE(test_y, y_pre1)**0.5
+    r1 = model.score(train_x, test_y)
+    # print(model.best_params_)
+    print(f"SVR for mission1 —— RMSE:{rmse1} R-squared:{r1}")
 
-# split to train and test dataset
-train_data = combine_data_final.sample(frac=0.7, random_state=0, axis=0)
-test_data = combine_data_final[~combine_data_final.index.isin(train_data.index)]
+# writer = SummaryWriter("../NNmodel")
+df = pd.read_csv('../data/final/data.csv')
+df['runtime'] = df['runtime'].fillna(0)
 
-learning_rate = 0.0001
-epochs = 40
-batch_size = 500
-num_batch = combine_data_final.shape[0] / batch_size
-features_num = combine_data_final.shape[1]
+X = df.drop(columns=['score'])
+y = df['score']
+X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.7)
 
-train_x = train_data.values[:, :-1]
-train_y = train_data.values[:, -1]
-tensor_x = torch.Tensor(train_x)
-tensor_y = torch.Tensor(train_y)
-torch_train = Data.TensorDataset(tensor_x, tensor_y)
-train_loader = Data.DataLoader(
-    dataset=torch_train,
-    batch_size=batch_size,
-    shuffle=True
-)
+# Model.train(X_train, y_train, X_test, y_test)
 
+# Model.linear_regression_task(X_train, y_train, X_test, y_test)
+svr_task(X_train, y_train, X_test, y_test)
 
-model = Net(n_input=features_num-1)
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.0001)
-loss_func = nn.MSELoss()
-
-for ep in range(epochs):
-    for sample_feature, score in train_loader:
-        y_hat = model(sample_feature)
-        loss = loss_func(y_hat, score)
-
-        optimizer.zero_grad()
-        loss.backward()
-
-        optimizer.step()
-    print(f'Epoch:{ep}, loss: {loss.data}')
-
-print("Finished Training")
